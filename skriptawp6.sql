70808036c8c473cc3c728d78076014d70e85b511
Simpf56 <xsorex561@gmail.com>
2024-11-13 17:45:29 +0100

    Završeno kreiranje prema ERA diagramu

---
+++ skriptawp6.sql
@@ -0,0 +1,42 @@
+﻿-- Ovo je SQL skripta
+
+use master;
+go --create database edunovawp6;
+
+drop database if exists edunovawp6;
+go
+
+create database edunovawp6;
+go
+
+use edunovawp6;
+go
+
+create table smjerovi(
+	sifra int not null primary key identity(1,1),
+	naziv varchar(50) not null,
+	cijena decimal(18,2) null, -- null se ne mora pisati
+	izvodiseod datetime,
+	vaucer bit
+);
+
+create table grupe(
+	sifra int not null primary key identity(1,1),
+	naziv varchar (20) not null,
+	velicinagrupe int not null,
+	predavac varchar (50),
+	smjer int not null references smjerovi(sifra)
+);
+
+create table polaznici(
+	sifra int not null primary key identity(1,1),
+	ime varchar (50) not null,
+	prezime varchar (50) not null,
+	email varchar (100) not null,
+	oib char (11)
+);
+
+create table clanovi(
+	grupa int not null references grupe(sifra),
+	polaznik int not null references polaznici(sifra)
+);
