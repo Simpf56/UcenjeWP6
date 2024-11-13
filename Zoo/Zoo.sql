3be363704e644e2f14227e812a90e9bbde657217
Simpf56 <xsorex561@gmail.com>
2024-11-13 18:00:04 +0100

    Prvi dio Zoo tablice

---
+++ Zoo/Zoo.sql
@@ -0,0 +1,28 @@
+﻿use master;
+go
+
+drop database Zoo if exists;
+
+create database Zoo;
+go
+
+use Zoo;
+go
+
+create table Zivotinja(
+sifra int not null primary key(1,1),
+vrsta varchar(50),
+ime varchar(50),
+djelatnik int,
+prostorija int,
+datum int
+);
+
+create table djelatnik(
+sifra int not null primary key(1,1),
+ime varchar(50),
+prezime varchar(50),
+iban varchar (50)
+);
+
+create table 
