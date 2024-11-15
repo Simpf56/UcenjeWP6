d30f6ceabd69a3158b094e746c3ab93253b29acc
Simpf56 <xsorex561@gmail.com>
2024-11-15 16:49:47 +0100

    era dijagram
    domaca zadaca 15/11/24

---
+++ era_dijagram2.sql
@@ -0,0 +1,36 @@
+﻿
+use master;
+go
+
+drop database if exists era_dijagram2;
+go
+
+create database era_dijagram2;
+go
+
+use era_dijagram2;
+
+create table osobe(
+	sifra int not null primary key identity (1,1),
+	ime varchar (50),
+	prezime varchar (50),
+	adresa varchar (50),
+	email varchar (50)
+);
+create table clan(
+	sifra int not null primary key identity (1,1),
+	clbroj int
+);
+create table vlasnik(
+	sifra int not null primary key identity (1,1),
+	knjiga int
+);
+create table knjige(
+	sifra int not null primary key identity (1,1),
+	naslov varchar (50),
+	pisac varchar (50),
+	vlasnik int foreign key references vlasnik(sifra),
+	clan int foreign key references clan(sifra),
+	datumpos datetime,
+	datumvrac datetime
+);
