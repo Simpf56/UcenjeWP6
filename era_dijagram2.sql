ed8e7a01d625270cc53d85bc7d94a77afabfb380
Simpf56 <xsorex561@gmail.com>
2024-11-15 16:57:46 +0100

    update era_dijagram2.sql
    dodan not null,
    i popravit error

--- era_dijagram2.sql
+++ era_dijagram2.sql
@@ -5,17 +5,30 @@ go
 drop database if exists era_dijagram2;
 go
 
+drop table if exists osobe;
+go
+
+drop table if exists clan;
+go
+
+drop table if exists vlasnik;
+go
+
+drop table if exists knjige;
+go
+
 create database era_dijagram2;
 go
 
 use era_dijagram2;
+go
 
 create table osobe(
 	sifra int not null primary key identity (1,1),
-	ime varchar (50),
-	prezime varchar (50),
+	ime varchar (50) not null,
+	prezime varchar (50) not null,
 	adresa varchar (50),
-	email varchar (50)
+	email varchar (50) not null
 );
 create table clan(
 	sifra int not null primary key identity (1,1),
@@ -27,10 +40,10 @@ create table vlasnik(
 );
 create table knjige(
 	sifra int not null primary key identity (1,1),
-	naslov varchar (50),
-	pisac varchar (50),
-	vlasnik int foreign key references vlasnik(sifra),
-	clan int foreign key references clan(sifra),
+	naslov varchar (50) not null,
+	pisac varchar (50) not null,
+	vlasnik int not null foreign key references vlasnik(sifra),
+	clan int not null foreign key references clan(sifra),
 	datumpos datetime,
 	datumvrac datetime
 );
