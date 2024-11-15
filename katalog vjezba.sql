c9ef075607a4fdc26398bbe2c7de52c9394793fa
Simpf56 <xsorex561@gmail.com>
2024-11-15 16:18:27 +0100

    katalog_vjezba.sql
    vjezba gotova

---
+++ katalog vjezba.sql
@@ -0,0 +1,39 @@
+﻿
+use master;
+go
+
+drop database if exists katalog;
+go
+
+create database katalog;
+go
+
+use katalog;
+
+create table autor(
+	sifra int not null primary key identity (1,1),
+	ime nvarchar(50) not null,
+	prezime nvarchar(50) not null,
+	datum_rodenja datetime
+);
+
+create table izdavac(
+	sifra int not null primary key identity (1,1),
+	naziv nvarchar(50) not null,
+	aktivan bit
+);
+
+create table mjesto(
+	sifra int not null primary key identity (1,1),
+	naziv nvarchar (50) not null,
+	postanski_broj nvarchar(10) not null,
+	drzava nvarchar(50)
+);
+
+create table katalog(
+	sifra int not null primary key identity (1,1),
+	autor int not null foreign key references autor(sifra),
+	naslov nvarchar (50) not null,
+	izdavac int not null foreign key references izdavac(sifra),
+	mjesto int not null foreign key references mjesto(sifra)
+	);
