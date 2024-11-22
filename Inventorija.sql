926518a98a47e96ab7176c3c2be987ccf0524704
Simpf56 <xsorex561@gmail.com>
2024-11-22 17:10:55 +0100

    Update Inventorija.sql
    dodani inserti

--- Inventorija.sql
+++ Inventorija.sql
@@ -8,9 +8,12 @@ create database Inventorija collate Croatian_CI_AS;;
 go
 
 use Inventorija;
+go
 
 create table Nabavljači(
 	sifra int not null primary key identity (1,1),
+	ime varchar (100) not null,
+	prezime varchar (100) not null,
 	naziv varchar (100) not null,
 	kontakt varchar (100) not null,
 	br_tel varchar (100) not null
@@ -34,8 +37,8 @@ create table Kupci(
 
 create table Narudžbe(
 	sifra int not null primary key identity (1,1),
-	ukupan_iznos int not null,
-	datum datetime,
+	ukupan_iznos decimal (22,2) not null,
+	datum date,
 	status varchar (20),
 	kupac int foreign key references Kupci(sifra)
 );
@@ -55,3 +58,30 @@ create table Stavke_narudžbe(
 	kolicina int not null,
 	cijena decimal (22,2)
 );
+
+insert into Nabavljači ( naziv,ime,prezime,kontakt,br_tel)
+values ('Dobavljač d.o.o.','Ivan','Perić','ivan.peric@dobavljac.hr',0911234567),
+('Global Supplies Ltd.','Petra','Novak','petra.novak@global.com',0917654321);
+
+insert into Proizvodi ( naziv,cijena)
+values ('T-Shirt',20.00),
+('Laptop',4500.00),
+('Kava',60.00);
+
+insert into Kupci (ime,prezime,br_tel,adresa,datum_rod)
+values ('Marko','Horvat',0912345678,'Ulica Marka 5, Zagreb','1990-05-15'),
+('Ana','Kovačić',0918765432,'Ulica Ane 10, Split','1985-12-20');
+
+insert into Narudžbe (ukupan_iznos,datum,status)
+values (100,00,'2024-11-22','Poslana'),
+(60,00,'2024-11-21','U tijeku');
+
+insert into Inventorija (kolicina_u_skladistu,lokacija,datum_promijene)
+values (100,'Zagreb','2024-11-15'),
+(50,'Split','2024-11-10'),
+(200,'Zagreb','2024-11-12');
+
+insert into Stavke_narudžbe (proizvod,narudzba,kolicina,cijena)
+values (1,1,2,40.00),
+(1,3,1,60.00),
+(2,3,1,60.00);
