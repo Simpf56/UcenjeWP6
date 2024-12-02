608f6999e0dbb0b3482537acfa79e7f38cc1cd70
Simpf56 <xsorex561@gmail.com>
2024-12-02 17:51:53 +0100

    Upoznavanje s sintaksom

--- CSHARP/Ucenje/Program.cs
+++ CSHARP/Ucenje/Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//komentar jedna linija
+
+/*
+ 
+Komentar više linija
+
+*/
+
+// See https://aka.ms/new-console-template for more information
+
+Console.WriteLine("Pozdrav svijetu lklkl člčl");
+Console.WriteLine("Hello Osijek");
