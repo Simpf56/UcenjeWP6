5596be4d850ea773f1abf4e459408c87ce220d36
Simpf56 <xsorex561@gmail.com>
2024-12-03 18:52:41 +0100

    TreningKuci_CSHARP

---
+++ TreningKuci/MojProjekat/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Kako se zoveš?");
+        string ime = Console.ReadLine();
+
+        Console.WriteLine("Kako se prezivaš?");
+        string prezime = Console.ReadLine();
+
+        Console.WriteLine("Koliko imaš godina?");
+        int godina = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("Ti se zoveš " + ime + " " + prezime + " i imas " + godina + " godina.");
+    }
+}
