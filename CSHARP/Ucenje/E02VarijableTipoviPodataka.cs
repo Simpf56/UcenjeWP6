171c94a506033f22e241e4e48eb7297ab1472df7
Simpf56 <xsorex561@gmail.com>
2024-12-04 17:48:03 +0100

    Varijable

--- CSHARP/Ucenje/E02VarijableTipoviPodataka.cs
+++ CSHARP/Ucenje/E02VarijableTipoviPodataka.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading.Channels;
 using System.Threading.Tasks;
 
 namespace Ucenje
@@ -25,7 +26,35 @@ namespace Ucenje
             i = int.Parse(Console.ReadLine());
             //korištenje vrijednosti
             Console.WriteLine("Upisali ste u {0}, i evo ga još jednom {1}", i,i);
-            
+
+            // bit logicka vrijednost 
+
+            bool logickaVrijednost;
+
+            Console.Write("Unesi True ili False: ");
+
+            logickaVrijednost = bool.Parse(Console.ReadLine());
+
+            Console.WriteLine("Unijeli ste {0}", logickaVrijednost);
+
+            // float - decimalni broj
+
+            Console.Write("Unesi broj (, za decimalni dio): ");
+
+            Console.WriteLine(float.Parse(Console.ReadLine()));
+
+            float broj = 3.14f; // f zato sto je to float
+
+            Console.WriteLine("Unesi ime: ");
+
+            string ime = Console.ReadLine();
+
+            Console.WriteLine("Unio sam \n{0}\n{1}\n{2}\n{3}",
+                i, logickaVrijednost, broj, ime);
+
+
+
+
         }
 
 
