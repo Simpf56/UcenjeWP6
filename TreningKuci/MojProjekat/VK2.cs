1cf8f053fa8f99d14af9e9db275a25e1e3500f28
Simpf56 <xsorex561@gmail.com>
2024-12-07 14:33:28 +0100

    TreningKuci_Operatori&IF

--- TreningKuci/MojProjekat/VK2.cs
+++ TreningKuci/MojProjekat/VK2.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Linq;
 
 namespace MojProjekat
 {
@@ -10,7 +11,81 @@ namespace MojProjekat
     {
         public static void VjezbaKuci()
         {
-            Console.WriteLine("Hello There");
-        }
+            //Console.WriteLine("Hello There");
+
+            int i = 1;
+            bool uvjet = i > 0;
+
+            if (uvjet)
+            {
+                Console.WriteLine("veće od 0");
+            }
+
+            if (i > 0)
+            {
+                Console.WriteLine("veće od 0");
+            }
+
+            if (uvjet)
+            {
+                Console.WriteLine("veće od 0");
+            }
+            else
+            {
+                Console.WriteLine("NIJE veće od 0");
+            }
+
+            if (uvjet)
+            {
+                Console.WriteLine("Veće od 0");
+            }
+            else if (i== -1)
+            {
+                Console.WriteLine("-1");
+            }
+            else
+            {
+                Console.WriteLine("Nije veće od 0");
+            }
+
+            
+            if (i > 0)
+            {
+                int p = -2;
+                if (p < 0)
+                {
+                    Console.WriteLine("Osijek");
+                }
+            }
+
+            if (i > 0)
+            {
+                Console.WriteLine("veće od 0");                
+            }
+            else
+            {
+                Console.WriteLine("NIJE veće od 0");
+            }
+
+            Console.WriteLine(i > 0 ? "veće od 0" : "NIJE veće od 0");
+
+            int x = 1, y = 0;
+
+            if(x != 1 & y > 0)
+            {
+                Console.WriteLine("Osijek"); 
+            }
+
+
+
+
+
+
+
+
+
+
+
+        }   
     }
 }
